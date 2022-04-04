a0dc59eccbe856ecfcac5e910aeb633f49273938
hulien22 <hejulien22@gmail.com>
2022-04-03 18:00:07 -0700

    final

--- src/Menu.gd
+++ src/Menu.gd
@@ -2,5 +2,6 @@ extends Node2D
 
 func _ready():
 	$wiz.update_color(Color(1,1,1),Color(1,1,1),Color(1,1,1),true)
+	$Fort2.get_node("HealthBar").hide()
 func _on_Button_pressed():
 	get_tree().change_scene("res://src/Map.tscn")
